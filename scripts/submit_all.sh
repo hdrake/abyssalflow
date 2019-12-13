16e0a3abb31255f6a2d52b33b41ff0e4106a77a2
hdrake <henrifdrake@gmail.com>
2019-12-13 10:53:23 -0500

    Added sensitivity experiments and new fig

--- scripts/submit_all.sh
+++
@@ -1,6 +0,0 @@
-module load julia/1.2.0
-sbatch slurm-pgcm-ridge-0.0_and_0.2 equil 10 100
-sbatch slurm-pgcm-ridge-0.4_and_0.8 equil 10 100
-sbatch slurm-pgcm-ridge-0.6_and_a-sens equil 10 100
-sbatch slurm-pgcm-ridge-0.6_r-sens equil 10 100
-
